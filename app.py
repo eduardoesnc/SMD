cebc0619d5875a5779f08dc50a65e7094069bb82
Eduardo Estevão Nunes Cavalcante <79337612+eduardoesnc@users.noreply.github.com>
2022-12-04 15:46:47 -0300

    Normalização das idades

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 st.set_page_config(
-    page_title = "Teste do Streamlit por Gabriel Alves",
+    page_title = "Teste do Streamlit pelo grupo SMD",
     layout = "wide",
     menu_items = {
         'About': "TESTE DO ABOUT"
