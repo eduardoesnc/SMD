6062a78a22aecd2336e7ef030dc00703efab275a
Eduardo Estevão Nunes Cavalcante <79337612+eduardoesnc@users.noreply.github.com>
2022-12-04 16:58:11 -0300

    Adição do Panda Profile

--- profiling.py
+++ profiling.py
@@ -4,8 +4,7 @@ from pandas_profiling.utils.cache import cache_file
 
 def readData():
     dataset = pd.read_csv('./data/train.csv')
-    # Normalizar valores das idades
-    dataset['age_of_car'] = round(dataset['age_of_car'].mul(10))
+    # Normalizar valores da idade
     dataset['age_of_policyholder'] = round(dataset['age_of_policyholder'].mul(100))
     return dataset
 bf = readData()
